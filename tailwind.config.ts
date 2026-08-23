b86aa9c352cbfa4ccb4d1c056b20909d80ab3d25
Deolindo Francisco <deolindo9@gmail.com>
2026-08-23 12:25:48 +0100

    up

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -48,7 +48,7 @@ export default {
         'header': 'var(--rcp-header-height)',
       },
       boxShadow: {
-        soft: '0 1px 2px rgba(15, 23, 42, 0.06), 0 1px 3px rgba(15, 23, 42, 0.08)',
+        soft: 'var(--rcp-shadow)',
       },
     },
   },
